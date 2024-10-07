af3b8cea85c5dcc15c400fc723513472c8474ecb
PrateekP <prateekpanta1789@gmail.com>
2024-10-07 20:32:11 +0530

    Chore: added ability to open Add balance tab

--- ExpenseTrackerApp/logic.js
+++ ExpenseTrackerApp/logic.js
@@ -2,28 +2,67 @@ const balanceAmount = document.getElementById("in-num");
 const spendAmount = document.getElementById("out-num");
 const optionPanel = document.querySelector(".options");
 const btnGroup = [...document.getElementsByClassName("btn")];
+const functionTab = document.querySelector(".func-tab");
 
-/* function checkContext(btnGroup) {
-    if (btnGroup[0].classList.contains("open")) {
-        const addNew = document.createElement('DIV');
-        addNew.setAttribute('class', 'new-blnc');
-        optionPanel.appendChild(addNew);
-    }
-} */
+let isBlncActive = false;
+let blncConfrm = false;
+let isExpsActive = false;
+
+function initBlncTab(container) {
+    const heading = document.createElement('H2');
+    heading.textContent = 'Enter Amount';
+    const input = document.createElement('INPUT');
+    input.setAttribute('type', 'number');
+    input.setAttribute('name', 'num');
+    input.setAttribute('placeholder', '0.00');
+        
+    container.appendChild(heading);
+    container.appendChild(input);
+    container.classList.add('func-active');
+}
 
 btnGroup.forEach(el => {
     el.addEventListener("click", () => {
         optionPanel.classList.toggle("active");
         el.classList.toggle("open");
 
-        checkContext(btnGroup);
+        if (el == btnGroup[0] && !isBlncActive) {
+            initBlncTab(functionTab);
+            isBlncActive = true;
+        }
+        else if (el == btnGroup[0] && isBlncActive) {
+            functionTab.innerHTML = '';
+            functionTab.classList.remove('func-active');
+            isBlncActive = false;
+        }
     })
 });
 
+/* function addBalance() {
+    if (!addBalanceInterface) {
+        const interfaceDiv = document.createElement('DIV');
+        interfaceDiv.setAttribute('class', 'new-blnce');
 
+        const heading = document.createElement('H2');
+        heading.textContent = 'Enter Amount';
 
+        const input = document.createElement('INPUT');
+        input.setAttribute('type', 'number');
+        input.setAttribute('name', 'blncAmt');
+        input.setAttribute('placeholder', '0.00');
 
+        optionPanel.appendChild(div);
+        div.appendChild(header);
+        div.appendChild(input);
 
-
-
-
+        btnGroup[0].textContent = "Cancel";
+        addBalanceInterface = true;
+    }
+    else if (addBalanceInterface) {
+        interfaceDiv.remove();
+        btnGroup[0].textContent = "Add Balance";
+        addBalanceInterface = false;
+    }
+    const textNode = document.createTextNode('Enter Amount');
+    header.appendChild(textNode);
+} */
